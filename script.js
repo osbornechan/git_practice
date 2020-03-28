0c75c1ca05218282d7cacf82924e0876a897e5e3
osbornechan <osbornecab@gmail.com>
2020-03-29 02:47:42 +0800

    Change win condition logic

--- script.js
+++ script.js
@@ -41,8 +41,6 @@ appendColorBox();
 
 //Player select colors (Drag and Drop)
 function onDragStart (event) {
-    console.log('enter ondragstart')
-    console.log(event.target.id)
     event.dataTransfer.setData('text', event.target.id);
     event.dataTransfer.effectAllowed = 'copy'; // WHY DOES THIS NOT WORK????
 }
@@ -60,25 +58,61 @@ function onDrop (event) {
 
 
 var currentRowArray = [];
+var tinySlotArraySorted = [];
 var gameTurn = 1;
 
 //Map game row to currentRowArray
 function populateArray () {
-    console.log('entered populateArray')
     for (var l = 1; l < 5; l++) {
         var selectId = '#r' + gameTurn + 'c' + l;
-        console.log('selectId: ' + selectId)
         var currentSlot = document.querySelector(selectId).childNodes;
-        console.log('currentRow: ' + currentSlot)
         var readSlot = currentSlot[1].style.backgroundColor;
-        console.log('readSlot: ' + readSlot)
         currentRowArray.push(readSlot);
-        console.log(currentRowArray)
     }
 }
 
 //Compare the arrays
 function checkAnswer () {
+    var tinySlotArray = [];
+
+    for (var m = 0; m < answerCombi.length; m++) {
+        if (m === 0) {
+            //Correct color & position
+            if (answerCombi[0] === currentRowArray[0]) {
+                tinySlotArray.push('black');
+            //Correct color but wrong position
+            } else if (answerCombi[0] === currentRowArray[1] || answerCombi[0] === currentRowArray[2] || answerCombi[0] === currentRowArray[3]) {
+                tinySlotArray.push('grey');
+            }
+        } else if (m === 1) {
+            //Correct color & position
+            if (answerCombi[1] === currentRowArray[1]) {
+                tinySlotArray.push('black');
+            //Correct color but wrong position
+            } else if (answerCombi[1] === currentRowArray[0] || answerCombi[1] === currentRowArray[2] || answerCombi[1] === currentRowArray[3]) {
+                tinySlotArray.push('grey');
+            }
+        } else if (m === 2) {
+            //Correct color & position
+            if (answerCombi[2] === currentRowArray[2]) {
+                tinySlotArray.push('black');
+            //Correct color but wrong position
+            } else if (answerCombi[2] === currentRowArray[0] || answerCombi[2] === currentRowArray[1] || answerCombi[2] === currentRowArray[3]) {
+                tinySlotArray.push('grey');
+            }
+        } else if (m === 3) {
+            //Correct color & position
+            if (answerCombi[3] === currentRowArray[3]) {
+                tinySlotArray.push('black');
+            //Correct color but wrong position
+            } else if (answerCombi[3] === currentRowArray[0] || answerCombi[3] === currentRowArray[1] || answerCombi[3] === currentRowArray[2]) {
+                tinySlotArray.push('grey');
+            }
+        }
+    }
+
+    tinySlotArraySorted = tinySlotArray.sort()
+
     var slot1 = '#r' + gameTurn + 'tc1';
     var slot2 = '#r' + gameTurn + 'tc2';
     var slot3 = '#r' + gameTurn + 'tc3';
@@ -89,78 +123,64 @@ function checkAnswer () {
     var tinySlot3 = document.querySelector(slot3);
     var tinySlot4 = document.querySelector(slot4);
 
-    for (var m = 0; m < answerCombi.length; m++) {
-        for (var n = 0; n < currentRowArray.length; n++) {
-            if (m === 0) {
-                //Correct color & position
-                if (answerCombi[0] === currentRowArray[0]) {
-                    tinySlot1.style.backgroundColor = 'black';
-                //Correct color but wrong position
-                } else if (answerCombi[0] === currentRowArray[n] && answerCombi[0] !== currentRowArray[0]) {
-                    tinySlot1.style.backgroundColor = 'grey';
-                }
-            } else if (m === 1) {
-                //Correct color & position
-                if (answerCombi[1] === currentRowArray[1]) {
-                    tinySlot2.style.backgroundColor = 'black';
-                //Correct color but wrong position
-                } else if (answerCombi[1] === currentRowArray[n] && answerCombi[1] !== currentRowArray[1]) {
-                    tinySlot2.style.backgroundColor = 'grey';
-                }
-            } else if (m === 2) {
-                //Correct color & position
-                if (answerCombi[2] === currentRowArray[2]) {
-                    tinySlot3.style.backgroundColor = 'black';
-                //Correct color but wrong position
-                } else if (answerCombi[2] === currentRowArray[n] && answerCombi[2] !== currentRowArray[2]) {
-                    tinySlot3.style.backgroundColor = 'grey';
-                }
-            } else if (m === 3) {
-                //Correct color & position
-                if (answerCombi[3] === currentRowArray[3]) {
-                    tinySlot4.style.backgroundColor = 'black';
-                //Correct color but wrong position
-                } else if (answerCombi[3] === currentRowArray[n] && answerCombi[3] !== currentRowArray[3]) {
-                    tinySlot4.style.backgroundColor = 'grey';
-                }
-            }
-        }
-    }
-    clearRowArray();
+    tinySlot1.style.backgroundColor = tinySlotArraySorted[0];
+    tinySlot2.style.backgroundColor = tinySlotArraySorted[1];
+    tinySlot3.style.backgroundColor = tinySlotArraySorted[2];
+    tinySlot4.style.backgroundColor = tinySlotArraySorted[3];
 }
 
-//Clear currentRowArray
-function clearRowArray () {
+//Clear arrays for next row
+function clearArrays () {
     for (var p = 0; p < 4; p++) {
         currentRowArray.pop();
+        tinySlotArraySorted.pop();
     }
+}
+
+//Check the 4 tiny slots for win condition
+function winOrLose () {
+    if (tinySlotArraySorted[0] === 'black' && tinySlotArraySorted[1] === 'black' && tinySlotArraySorted[2] === 'black' && tinySlotArraySorted[3] === 'black') {
+        alert('You win!');
+    } else if (gameTurn === 8) {
+        alert('You lose!');
+    }
+
+    clearArrays();
 }
 
 function trackGameState () {
     if (gameTurn === 1) {
         populateArray();
         checkAnswer();
+        winOrLose();
     } else if (gameTurn === 2) {
         populateArray();
         checkAnswer();
+        winOrLose();
     } else if (gameTurn === 3) {
         populateArray();
         checkAnswer();
+        winOrLose();
     } else if (gameTurn === 4) {
         populateArray();
         checkAnswer();
+        winOrLose();
     } else if (gameTurn === 5) {
         populateArray();
         checkAnswer();
+        winOrLose();
     } else if (gameTurn === 6) {
         populateArray();
         checkAnswer();
+        winOrLose();
     } else if (gameTurn === 7) {
         populateArray();
         checkAnswer();
+        winOrLose();
     } else if (gameTurn === 8) {
         populateArray();
         checkAnswer();
+        winOrLose();
     }
     gameTurn++;
 }
@@ -180,4 +200,4 @@ function cloneSelectedPegs () {
     colorBox.appendChild(clonedPeg);
 }
 
-colorBox.addEventListener('click', cloneSelectedPegs);
+colorBox.addEventListener('dragstart', cloneSelectedPegs);
